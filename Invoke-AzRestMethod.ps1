f35370a0fd22b9d9cd69d370d86b2e4750fd39b2
Sawal72 <github@w-online.nl>
2022-09-02 12:35:44 +0200

    Add files via upload
    Bug fix

---
+++ Invoke-AzRestMethod.ps1
@@ -0,0 +1,40 @@
+function Invoke-AzRestMethod {
+    [CmdletBinding(SupportsShouldProcess=$True)] #adds Confirm and WhatIf options as parameter options and so forth 
+    param(
+        [parameter(Mandatory=$true)]
+        [PSObject]$token
+        ,
+        [ValidateSet('Get','Post')]
+        [string]$Method = 'Get'
+        ,
+        [System.Uri]$URI
+        ,
+        [switch]$Raw
+    )
+
+    $params = @{
+        ContentType = 'application/x-www-form-urlencoded'
+        Headers = @{
+            'Authorization' = "Bearer $($token.AccessToken)"
+        }
+        Method = $Method
+        URI = $URI
+    }
+
+    [System.Collections.ArrayList]$List = @()
+
+    If ($raw){
+        Invoke-RestMethod @params -TimeoutSec 60 -Verbose:$false
+        
+    }
+    Else{
+        # (Invoke-RestMethod @params -TimeoutSec 60 -Verbose:$false).Value 
+        
+        do {
+            $result = Invoke-RestMethod @params -TimeoutSec 60
+            $List.AddRange($result.value)
+            $Params.URI = $result.nextLink
+        } until ($Params.URI -eq $null)
+        return $list
+    }
+}
